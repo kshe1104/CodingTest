e4b86f01204b3dfc7a1454780a9436fb4cb1bdcf
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-04-10 12:32:34 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,21 @@
+# -- 코드를 작성해주세요
+# select id, fish_name, fish_info.length as length
+# from fish_info join fish_name_info on fish_info.fish_type = fish_name_info.fish_type
+# where fish_info.fish_type in
+# (
+#     select fish_type
+#     from fish_info
+#     group by fish_type
+#     having length = max(length)
+# )
+
+# order by id
+
+select id, fish_name,length
+from fish_info join fish_name_info on fish_info.fish_type = fish_name_info.fish_type
+where fish_info.fish_type in
+(
+select fish_type
+from fish_info
+group by fish_type
+having length = max(length))
