b1923839c2c473dfec024c7eaa9b31f828b887a5
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-04-15 15:17:57 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+
+        for(int i=0;i<9;i++) // 2차원 배열 입력
+        {
+            for(int j=0;j<9;j++)
+            {
+                arr[i][j] = sr.nextInt();
+            }
+        }
+        int max = arr[0][0];
+        int a=0,b=0;
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(max<arr[i][j])
+                {
+                    max = arr[i][j];
+                a=i;
+                b=j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((a+1)+" "+(b+1));
+    }
+}
