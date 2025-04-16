60f1af7cfb75179cd3fb98971976fd3fe5b17c7b
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-04-16 14:40:48 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.10 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String[] answer2 = new String[n];
+        String[] result = new String[n];
+        
+        for(int i=0;i<n;i++){
+            answer[i] = Integer.toBinaryString(arr1[i]);
+            answer2[i] = Integer.toBinaryString(arr2[i]);
+            result[i] = ""; //초기화
+        
+        
+        while(answer[i].length()<n){
+            answer[i]="0"+answer[i];
+        }
+        while(answer2[i].length()<n){
+            answer2[i]="0"+answer2[i];
+        }
+        for(int j=0;j<n;j++){
+            if(answer[i].charAt(j)=='1'|| answer2[i].charAt(j)=='1'){
+                result[i]+="#";
+            }
+            else if(answer[i].charAt(j)=='0'||answer2[i].charAt(j)=='0'){
+                result[i]+=" ";
+            }
+        }
+        }
+        return result;
+    }
+}
