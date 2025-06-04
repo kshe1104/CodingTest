f2ff1e0fd2e24244376bf91957ea29376f444159
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-06-04 13:36:32 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,13 @@
+select
+it.item_id,
+ii.item_name,
+ii.rarity
+from
+item_info as ii
+inner join item_tree as it
+on ii.item_id = it.item_id
+where it.parent_item_id in (select item_id
+                           from item_info
+                           where rarity = 'RARE')
+                           order by
+                           it.Item_id desc
