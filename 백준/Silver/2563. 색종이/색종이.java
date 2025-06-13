a07aa72d99c781ad97bb7c2ced2b36f5ee44a854
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-06-13 13:29:59 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
-        int num = sr.nextInt(); //색종이 갯수
-        boolean[][] paper = new boolean[100][100];
+        
+        boolean[][] Square = new boolean[100][100];
         int area = 0;
-
-        for(int i=0;i<num;i++)
-        {
-            int x = sr.nextInt(); //입력
+        int num = sr.nextInt();
+        
+        for(int i=0;i<num;i++){
+            int x = sr.nextInt();
             int y = sr.nextInt();
 
-
-            for (int r = x; r < x+10; r++) {
-                for (int c = y; c < y + 10; c++) {
-                    if (!paper[r][c]) {
-                        paper[r][c] = true;
+            for(int j=x;j<x+10;j++){
+                for(int k=y;k<y+10;k++){
+                    if(!Square[j][k]){
+                        Square[j][k] = true;
                         area++;
                     }
                 }
             }
         }
         System.out.println(area);
-    }
 
+    }
 }
