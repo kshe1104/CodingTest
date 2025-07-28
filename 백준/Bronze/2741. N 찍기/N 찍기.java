dda79b61cf2d3a3cc3c9a7601a81573e11a4498c
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-07-28 14:19:09 +0900

    [Bronze V] Title: N 찍기, Time: 836 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sr = new Scanner(System.in);
+        int num = sr.nextInt();
+        for(int i=1;i<=num;i++){
+            System.out.println(i);
+        }
+    }
+}
