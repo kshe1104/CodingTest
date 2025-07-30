039f1bedacd9fe03448387ce4081cb7a0f27bd3d
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-07-30 15:00:09 +0900

    [Bronze IV] Title: R2, Time: 176 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sr=  new Scanner(System.in);
+        int r1 = sr.nextInt();
+        int s = sr.nextInt();
+        int result = 2 * s - r1;
+        System.out.println(result);
+        
+    }
+}
