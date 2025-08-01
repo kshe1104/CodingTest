1cf34107520e942115b29bd7c8a47e10502adcc6
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-08-01 15:17:27 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1708 ms, Memory: 192052 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -4,28 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int n = sr.nextInt(); //테스트 케이스 갯수 입력받기
 
-        int N = sr.nextInt();
-        Integer arr[][] = new Integer[N][2];
+        int[][] arr = new int[n][2];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i][0] = sr.nextInt();
             arr[i][1] = sr.nextInt();
         }
-        Arrays.sort(arr, new Comparator<Integer[]>() {
-            @Override
-            public int compare(Integer[] o1, Integer[] o2) {
-                if (o1[0].equals(o2[0])) {
-                    return o1[1] - o2[1];
-                }
-                else{
-                    return o1[0] - o2[0];
-                }
+        
+        Arrays.sort(arr,(e1,e2)->{
+            if (e1[0] == e2[0]) {
+                return e1[1]-e2[1];
+            }
+            else{
+                return e1[0] - e2[0];
             }
         });
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
