f206a43d943293206302de0f717bb7ea6bb2c5d6
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-08-01 15:39:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1292 ms, Memory: 186980 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,29 +1,35 @@
 
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
-        
+
         int n = sr.nextInt(); //테스트 케이스 갯수 입력받기
 
         int[][] arr = new int[n][2];
 
         for (int i = 0; i < n; i++) {
-            arr[i][0] = sr.nextInt();
+            arr[i][0] = sr.nextInt(); //좌표들 입력받기
             arr[i][1] = sr.nextInt();
         }
-        
+
         Arrays.sort(arr,(e1,e2)->{
             if (e1[0] == e2[0]) {
-                return e1[1]-e2[1];
+                return e1[1]-e2[1]; //첫번째 값이 같으면 두번째 값 기준으로 오름차순
             }
             else{
-                return e1[0] - e2[0];
+                return e1[0] - e2[0]; //아니면 첫번째 값 기준으로 오름차순
             }
         });
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
         }
+
+        System.out.println(sb);
+        
     }
 }
