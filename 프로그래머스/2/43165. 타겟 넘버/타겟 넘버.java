343f2532220e4fa0d9a3f29a59c0279c5046d529
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-08-14 14:20:48 +0900

    [level 2] Title: 타겟 넘버, Time: 4.71 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,17 @@
+class Solution {
+    int answer=0;
+    public int solution(int[] numbers, int target) {
+       dfs(numbers,0,target,0);
+        return answer;
+    }
+    
+    //깊이 우선 탐색
+    public void dfs(int[] numbers,int depth,int target,int sum){
+        if(depth==numbers.length){
+            if(target==sum) answer++;}
+            else{
+                dfs(numbers,depth+1,target,sum+numbers[depth]);
+                dfs(numbers,depth+1,target,sum-numbers[depth]);
+            }
+        }
+    }
