28237871717e8bb1246d5eaeb7833716b55e2f3d
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-08-22 15:28:21 +0900

    [Bronze IV] Title: 세 수, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,13 +1,22 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sr = new Scanner(System.in);
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
 
-        int[] arr = {sr.nextInt(), sr.nextInt(), sr.nextInt()};
-        sr.close();
-
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
@@ -19,5 +28,5 @@ public class Main {
         }
         System.out.println(arr[1]);
     }
-    
+
 }
