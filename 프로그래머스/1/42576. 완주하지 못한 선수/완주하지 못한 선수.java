ea083537f5818b98ef1fb5ad99a7c471aba3734f
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-08-31 15:22:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.98 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String,Integer> map = new HashMap<>();
+        
+        //참가자 전원 추가
+        for(String p : participant){
+            map.put(p,map.getOrDefault(p,0)+1);
+        }
+        
+        //완주한 사람제거
+        for(String c: completion){
+            int n = map.get(c)-1;
+            if(n==0) map.remove(c);
+            else map.put(c,n);
+        }
+        
+        return map.keySet().iterator().next();
+    }
+}
